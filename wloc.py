c3b771b94841b38ae27e9e980ffe7868dad5b8c2
V1TSK <vitaly@easycoding.org>
2016-01-01 23:16:02 +0500

    Will now generate XML file.

--- wloc.py
+++ wloc.py
@@ -6,11 +6,30 @@ gi.require_version('NMClient', '1.0')
 from gi.repository import NetworkManager, NMClient
 import xml.etree.cElementTree as ET
 
+APIKey = ''
+
+# Connecting to Network Manager...
 NMClient = NMClient.Client.new()
 NMDevices = NMClient.get_devices()
 
+# Generating base XML structure...
+xml = ET.Element("ya_lbs_request")
+
+# Filling API Keys...
+common = ET.SubElement(xml, "common")
+ET.SubElement(common, "version").text = "1.0"
+ET.SubElement(common, "api_key").text = APIKey
+
+# Creating wifi_networks element...
+networks = ET.SubElement(xml, "wifi_networks")
+
+# Retrieving available networks...
 for NMDevice in NMDevices:
     if NMDevice.get_device_type() == NetworkManager.DeviceType.WIFI:
         for AccessPoint in NMDevice.get_access_points():
-            print('%-30s (%s) %dMHz %d%%' % (AccessPoint.get_ssid(), AccessPoint.get_bssid(), AccessPoint.get_frequency(), AccessPoint.get_strength()))
+            network = ET.SubElement(networks, "network")
+            ET.SubElement(network, "mac").text = AccessPoint.get_bssid()
+            ET.SubElement(network, "signal_strength").text = str(int(AccessPoint.get_strength() / 2 - 100))
 
+# Saving XML...
+ET.ElementTree(xml).write('abc.xml', 'utf8')
