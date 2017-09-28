cd542fc44465a1ea843b769adf61d769866ec66e
Vitaly Zaitsev <vitaly@easycoding.org>
2017-09-28 22:34:43 +0500

    Added class WFLoc.

--- wloc/__init__.py
+++ wloc/__init__.py
@@ -20,74 +20,75 @@
 #
 
 
-def conv_strength(stp):
-    # Converting strength percents to RSSI (dBm)...
-    return '%.0f' % (stp / 2 - 100)
+class WFLoc:
+    @staticmethod
+    def conv_strength(stp):
+        # Converting strength percents to RSSI (dBm)...
+        return '%.0f' % (stp / 2 - 100)
 
+    def fetch_networks(self):
+        # Importing Network Manager from GI repository and other modules...
+        import gi, warnings
+        gi.require_version('NetworkManager', '1.0')
+        gi.require_version('NMClient', '1.0')
+        from gi.repository import NetworkManager, NMClient
 
-def fetch_networks():
-    # Importing Network Manager from GI repository and other modules...
-    import gi, warnings
-    gi.require_version('NetworkManager', '1.0')
-    gi.require_version('NMClient', '1.0')
-    from gi.repository import NetworkManager, NMClient
+        # Ignoring warnings on new Python versions...
+        warnings.filterwarnings('ignore')
 
-    # Creating new list for networks...
-    netlist = []
+        # Connecting to Network Manager...
+        nmclient = NMClient.Client.new()
+        nmdevices = nmclient.get_devices()
 
-    # Ignoring warnings on new Python versions...
-    warnings.filterwarnings('ignore')
+        # Retrieving available networks...
+        for nmdevice in nmdevices:
+            if nmdevice.get_device_type() == NetworkManager.DeviceType.WIFI:
+                for accesspoint in nmdevice.get_access_points():
+                    self.netlist.append([accesspoint.get_bssid(), self.conv_strength(accesspoint.get_strength())])
 
-    # Connecting to Network Manager...
-    nmclient = NMClient.Client.new()
-    nmdevices = nmclient.get_devices()
+    def query_yandex(self):
+        # Importing required modules...
+        import xml.etree.cElementTree as et
+        import requests as rq
 
-    # Retrieving available networks...
-    for nmdevice in nmdevices:
-        if nmdevice.get_device_type() == NetworkManager.DeviceType.WIFI:
-            for accesspoint in nmdevice.get_access_points():
-                netlist.append([accesspoint.get_bssid(), conv_strength(accesspoint.get_strength())])
+        # Generating base XML structure...
+        xml = et.Element('ya_lbs_request')
 
-    # Returning result...
-    return netlist
+        # Filling API Keys...
+        common = et.SubElement(xml, 'common')
+        et.SubElement(common, 'version').text = '1.0'
+        et.SubElement(common, 'api_key').text = self.apikey
 
+        # Creating wifi_networks element...
+        networks = et.SubElement(xml, 'wifi_networks')
 
-def query_yandex():
-    # Importing required modules...
-    import xml.etree.cElementTree as et
-    import requests as rq
+        # Retrieving available networks...
+        for arr in self.netlist:
+            network = et.SubElement(networks, 'network')
+            et.SubElement(network, 'mac').text = arr[0]
+            et.SubElement(network, 'signal_strength').text = arr[1]
 
-    # Setting constants...
-    apikey = ''
-    apiuri = 'https://api.lbs.yandex.net/geolocation'
+        try:
+            # Sending our XML file to API...
+            r = rq.post(self.apiuri, data={'xml': et.tostring(xml, 'utf8')})
 
-    # Generating base XML structure...
-    xml = et.Element('ya_lbs_request')
+            # Parsing XML response...
+            result = et.fromstring(r.content).findall('./position/')
 
-    # Filling API Keys...
-    common = et.SubElement(xml, 'common')
-    et.SubElement(common, 'version').text = '1.0'
-    et.SubElement(common, 'api_key').text = apikey
+            # Returning result...
+            return [result[0].text, result[1].text]
 
-    # Creating wifi_networks element...
-    networks = et.SubElement(xml, 'wifi_networks')
+        except:
+            # Exception detected...
+            print('Server returned code: %s.\n\nRaw output:\n%s\n' % (r.status_code, r.text))
 
-    # Retrieving available networks...
-    for arr in fetch_networks():
-        network = et.SubElement(networks, 'network')
-        et.SubElement(network, 'mac').text = arr[0]
-        et.SubElement(network, 'signal_strength').text = arr[1]
+    def __init__(self):
+        # Setting constants...
+        self.apikey = ''
+        self.apiuri = 'https://api.lbs.yandex.net/geolocation'
 
-    try:
-        # Sending our XML file to API...
-        r = rq.post(apiuri, data={'xml': et.tostring(xml, 'utf8')})
+        # Creating new list for networks...
+        self.netlist = []
 
-        # Parsing XML response...
-        result = et.fromstring(r.content).findall('./position/')
-
-        # Returning result...
-        return [result[0].text, result[1].text]
-
-    except:
-        # Exception detected...
-        print('Server returned code: %s.\n\nRaw output:\n%s\n' % (r.status_code, r.text))
+        # Retrieving available networks...
+        self.fetch_networks()
