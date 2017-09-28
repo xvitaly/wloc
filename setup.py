d8545d824b37132826f1a4a44aa73237e71b5a47
Vitaly Zaitsev <vitaly@easycoding.org>
2017-09-28 22:58:48 +0500

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='wloc',
+    version='0.2',
+    packages=['wloc'],
+    url='https://github.com/xvitaly/wloc',
+    license='GPLv3',
+    install_requires=['requests'],
+    author='Vitaly Zaitsev',
+    author_email='vitaly@easycoding.org',
+    description='Simple Wi-Fi geolocation tool'
+)
