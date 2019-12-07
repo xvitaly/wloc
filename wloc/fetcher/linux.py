da6f72a0cb555362956e236f46cd83a985c3d65a
Vitaly Zaitsev <vitaly@easycoding.org>
2019-12-07 16:24:45 +0100

    Added a new class FetcherLinux.

---
+++ wloc/fetcher/linux.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+
+#
+# Wi-Fi simple geolocation tool
+# Copyright (c) 2015 - 2019 EasyCoding Team
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from .abstract import Fetcher
+
+
+class FetcherLinux(Fetcher):
+    def __fetch_networks(self):
+        """
+        Connects to Network Manager, fetching list of available networks
+        and stores them in private class property.
+        """
+        # Importing Network Manager module...
+        from NetworkManager import NetworkManager, Wireless
+
+        # Using DBus to ask Network Manager for available networks...
+        for nmdevice in NetworkManager.GetDevices():
+            if type(nmdevice) == Wireless:
+                for accesspoint in nmdevice.AccessPoints:
+                    self.__netlist.append([accesspoint.HwAddress, self.conv_strength(accesspoint.Strength)])
+
+    @property
+    def networks(self):
+        self.__fetch_networks()
+        return self.__netlist
