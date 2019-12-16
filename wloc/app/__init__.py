a3ca0c15698f62257053f8921826c50873afd972
Vitaly Zaitsev <vitaly@easycoding.org>
2019-12-16 12:41:48 +0100

    Refactored application.
    Closes #46.

--- wloc/app/__init__.py
+++ wloc/app/__init__.py
@@ -17,3 +17,69 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+
+import argparse
+import os
+import wloc
+
+
+class WiFiLocatorApp:
+    def __parser_create(self):
+        self.__parser = argparse.ArgumentParser()
+        self.__parser_add_arguments()
+
+    def __parser_add_arguments(self):
+        self.__parser.add_argument('--yandex', '-y', help='Use Yandex Geolocation API.', action="store_true",
+                                   required=False)
+        self.__parser.add_argument('--google', '-g', help='Use Google Geolocation API.', action="store_true",
+                                   required=False)
+        self.__parser.add_argument('--mozilla', '-m', help='Use Mozilla Geolocation API.', action="store_true",
+                                   required=False)
+
+    def __parse_arguments(self):
+        self.__arguments = self.__parser.parse_args()
+
+    def __check_arguments(self):
+        return self.__arguments.yandex or self.__arguments.google or self.__arguments.mozilla
+
+    def __print_result(self, coords, service):
+        print('%s results:\nLatitude: %.6f\nLongitude: %.6f\n' % (service, coords[0], coords[1]))
+
+    def __print_error(self, service, message):
+        print('An error occurred while querying %s backend: %s' % (service, message))
+
+    def __call_yandex(self, name):
+        if self.__arguments.yandex:
+            try:
+                self.__print_result(self.__locator.query_yandex(), name)
+            except Exception as ex:
+                self.__print_error(name, ex)
+
+    def __call_google(self, name):
+        if self.__arguments.google:
+            try:
+                self.__print_result(self.__locator.query_google(), name)
+            except Exception as ex:
+                self.__print_error(name, ex)
+
+    def __call_mozilla(self, name):
+        if self.__arguments.mozilla:
+            try:
+                self.__print_result(self.__locator.query_mozilla(), name)
+            except Exception as ex:
+                self.__print_error(name, ex)
+
+    def run(self):
+        if self.__check_arguments():
+            self.__locator.fetch_networks()
+            self.__call_yandex('Yandex')
+            self.__call_google('Google')
+            self.__call_mozilla('Mozilla')
+        else:
+            self.__parser.print_help()
+
+    def __init__(self):
+        self.__parser_create()
+        self.__parse_arguments()
+        self.__locator = wloc.WiFiLocator(gg_apikey=os.getenv('APIKEY_GOOGLE'), ya_apikey=os.getenv('APIKEY_YANDEX'),
+                                          mm_apikey=os.getenv('APIKEY_MOZILLA'))
--- wloc/app/run.py
+++ wloc/app/run.py
@@ -18,69 +18,14 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import argparse
-import os
-import wloc
-
-
-def mkparser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--yandex', '-y', help='Use Yandex Geolocation API.', action="store_true", required=False)
-    parser.add_argument('--google', '-g', help='Use Google Geolocation API.', action="store_true", required=False)
-    parser.add_argument('--mozilla', '-m', help='Use Mozilla Geolocation API.', action="store_true", required=False)
-    return parser
-
-
-def show_result(coords, service):
-    print('%s results:\nLatitude: %.6f\nLongitude: %.6f\n' % (service, coords[0], coords[1]))
-
-
-def show_error(service, message):
-    print('An error occurred while querying %s backend: %s' % (service, message))
+from wloc.app import WiFiLocatorApp
 
 
 def main():
     try:
-        # Creating parser instance and fetching command-line arguments...
-        aparse = mkparser()
-        params = aparse.parse_args()
-
-        # Checking selected options...
-        if params.yandex or params.google or params.mozilla:
-
-            # Creating WFLoc object...
-            locator = wloc.WiFiLocator(gg_apikey=os.getenv('APIKEY_GOOGLE'), ya_apikey=os.getenv('APIKEY_YANDEX'),
-                                       mm_apikey=os.getenv('APIKEY_MOZILLA'))
-
-            # Getting list of available Wi-Fi networks...
-            locator.fetch_networks()
-
-            # Querying Yandex if selected...
-            if params.yandex:
-                try:
-                    show_result(locator.query_yandex(), 'Yandex')
-                except Exception as ex:
-                    show_error('Yandex', ex)
-
-            # Querying Google if selected...
-            if params.google:
-                try:
-                    show_result(locator.query_google(), 'Google')
-                except Exception as ex:
-                    show_error('Google', ex)
-
-            # Querying Mozilla if selected...
-            if params.mozilla:
-                try:
-                    show_result(locator.query_mozilla(), 'Mozilla')
-                except Exception as ex:
-                    show_error('Mozilla', ex)
-        else:
-            # No command-line arguments detected. Show small help...
-            aparse.print_help()
-
+        app = WiFiLocatorApp()
+        app.run()
     except Exception as ex:
-        # Exception detected...
         print('An error occurred while running application: %s' % ex)
 
 
