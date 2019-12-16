f9c3758028b4b6a51d9a1fc02287220ed7daa909
Vitaly Zaitsev <vitaly@easycoding.org>
2019-12-16 13:15:21 +0100

    Refactoring: reused code. Added logger.
    Closes #34.

--- wloc/app/__init__.py
+++ wloc/app/__init__.py
@@ -19,11 +19,21 @@
 #
 
 import argparse
+import logging
 import os
+import sys
+
 import wloc
 
 
 class WiFiLocatorApp:
+    def __setlogger(self):
+        self.__logger = logging.getLogger(__name__)
+        self.__logger.setLevel('INFO')
+        e_handler = logging.StreamHandler(sys.stdout)
+        e_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s'))
+        self.__logger.addHandler(e_handler)
+
     def __parser_create(self):
         self.__parser = argparse.ArgumentParser()
         self.__parser_add_arguments()
@@ -42,44 +52,39 @@ class WiFiLocatorApp:
     def __check_arguments(self):
         return self.__arguments.yandex or self.__arguments.google or self.__arguments.mozilla
 
-    def __print_result(self, coords, service):
-        print('%s results:\nLatitude: %.6f\nLongitude: %.6f\n' % (service, coords[0], coords[1]))
+    def __set_backends(self):
+        self.__locator = wloc.WiFiLocator(gg_apikey=os.getenv('APIKEY_GOOGLE'), ya_apikey=os.getenv('APIKEY_YANDEX'),
+                                          mm_apikey=os.getenv('APIKEY_MOZILLA'))
+        self.__selector = {
+            'Yandex': self.__locator.query_yandex,
+            'Google': self.__locator.query_google,
+            'Mozilla': self.__locator.query_mozilla
+        }
 
-    def __print_error(self, service, message):
-        print('An error occurred while querying %s backend: %s' % (service, message))
-
-    def __call_yandex(self, name):
+    def __get_results(self):
         if self.__arguments.yandex:
-            try:
-                self.__print_result(self.__locator.query_yandex(), name)
-            except Exception as ex:
-                self.__print_error(name, ex)
-
-    def __call_google(self, name):
+            self.__call_backend('Yandex')
         if self.__arguments.google:
-            try:
-                self.__print_result(self.__locator.query_google(), name)
-            except Exception as ex:
-                self.__print_error(name, ex)
-
-    def __call_mozilla(self, name):
+            self.__call_backend('Google')
         if self.__arguments.mozilla:
-            try:
-                self.__print_result(self.__locator.query_mozilla(), name)
-            except Exception as ex:
-                self.__print_error(name, ex)
+            self.__call_backend('Mozilla')
+
+    def __call_backend(self, name):
+        try:
+            coords = self.__selector[name]()
+            self.__logger.info('%s results:\nLatitude: %.6f\nLongitude: %.6f\n', name, coords[0], coords[1])
+        except Exception:
+            self.__logger.exception('An error occurred while querying %s backend.'.format(name))
 
     def run(self):
         if self.__check_arguments():
             self.__locator.fetch_networks()
-            self.__call_yandex('Yandex')
-            self.__call_google('Google')
-            self.__call_mozilla('Mozilla')
+            self.__get_results()
         else:
             self.__parser.print_help()
 
     def __init__(self):
+        self.__setlogger()
         self.__parser_create()
         self.__parse_arguments()
-        self.__locator = wloc.WiFiLocator(gg_apikey=os.getenv('APIKEY_GOOGLE'), ya_apikey=os.getenv('APIKEY_YANDEX'),
-                                          mm_apikey=os.getenv('APIKEY_MOZILLA'))
+        self.__set_backends()
