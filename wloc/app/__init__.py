b6df8921564847bc08b7aa8f1db2b8e271b34d50
Vitaly Zaitsev <vitaly@easycoding.org>
2019-12-16 14:12:37 +0100

    Added documentation for all members of App class.

--- wloc/app/__init__.py
+++ wloc/app/__init__.py
@@ -29,18 +29,27 @@ from .settings import Settings
 
 
 class App:
-    def __setlogger(self):
+    def __setlogger(self) -> None:
+        """
+        Configures logger for internal use.
+        """
         self.__logger = logging.getLogger(__name__)
         self.__logger.setLevel(Settings.log_level)
         e_handler = logging.StreamHandler(sys.stdout)
         e_handler.setFormatter(logging.Formatter(Settings.log_format))
         self.__logger.addHandler(e_handler)
 
-    def __parser_create(self):
+    def __parser_create(self) -> None:
+        """
+        Creates instance of command-line arguments parser.
+        """
         self.__parser = argparse.ArgumentParser()
         self.__parser_add_arguments()
 
-    def __parser_add_arguments(self):
+    def __parser_add_arguments(self) -> None:
+        """
+        Adds new supported options to command-line arguments parser.
+        """
         self.__parser.add_argument('--yandex', '-y', help=Messages.arg_desc_yandex, action="store_true",
                                    required=False)
         self.__parser.add_argument('--google', '-g', help=Messages.arg_desc_google, action="store_true",
@@ -48,13 +57,24 @@ class App:
         self.__parser.add_argument('--mozilla', '-m', help=Messages.arg_desc_mozilla, action="store_true",
                                    required=False)
 
-    def __parse_arguments(self):
+    def __parse_arguments(self) -> None:
+        """
+        Parses command-line arguments and provides a special object
+        to work with.
+        """
         self.__arguments = self.__parser.parse_args()
 
-    def __check_arguments(self):
+    def __check_arguments(self) -> bool:
+        """
+        Checks if at least one of optional command-line arguments present.
+        :return: Availability of any optional command-line arguments.
+        """
         return self.__arguments.yandex or self.__arguments.google or self.__arguments.mozilla
 
-    def __set_backends(self):
+    def __set_backends(self) -> None:
+        """
+        Creates instance of WiFiLocator class and special switch surrogate.
+        """
         self.__locator = WiFiLocator(gg_apikey=os.getenv('APIKEY_GOOGLE'), ya_apikey=os.getenv('APIKEY_YANDEX'),
                                      mm_apikey=os.getenv('APIKEY_MOZILLA'))
         self.__selector = {
@@ -63,7 +83,10 @@ class App:
             'Mozilla': self.__locator.query_mozilla
         }
 
-    def __get_results(self):
+    def __get_results(self) -> None:
+        """
+        Calls enabled by user backends.
+        """
         if self.__arguments.yandex:
             self.__call_backend('Yandex')
         if self.__arguments.google:
@@ -71,21 +94,31 @@ class App:
         if self.__arguments.mozilla:
             self.__call_backend('Mozilla')
 
-    def __call_backend(self, name):
+    def __call_backend(self, name: str) -> None:
+        """
+        Directly calls one of supported geolocation backend.
+        :param name:
+        """
         try:
             coords = self.__selector[name]()
             self.__logger.info(Messages.backend_result, name, coords[0], coords[1])
         except Exception:
             self.__logger.exception(Messages.backend_error.format(name))
 
-    def run(self):
+    def run(self) -> None:
+        """
+        Run application.
+        """
         if self.__check_arguments():
             self.__locator.fetch_networks()
             self.__get_results()
         else:
             self.__parser.print_help()
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """
+        Main constructor of App class.
+        """
         self.__setlogger()
         self.__parser_create()
         self.__parse_arguments()
