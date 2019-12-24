0e0a404a89bc1ea8710813a1d77d718c896d059c
Vitaly Zaitsev <vitaly@easycoding.org>
2019-12-24 14:22:20 +0100

    Fixed incorrect license tag.

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     description='Simple Wi-Fi geolocation library and tool',
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPLv3 License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6'
