de74d7edd8d66ca9ec9880ed6c7e16d9be9a4c23
Vitaly Zaitsev <vitaly@easycoding.org>
2021-05-07 17:30:20 +0200

    Added a new Python package for the custom exception types.

---
+++ wloc/exception/__init__.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+# SPDX-FileCopyrightText: 2015-2021 EasyCoding Team
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+class BackendError(Exception):
+    """
+    Base class for the API backend errors.
+    """
+    pass
