e1c646751a757235dc979bc8f8cbf49d2f7eda9d
Vitaly Zaitsev <vitaly@easycoding.org>
2021-05-08 20:21:48 +0200

    Added NativeWiFi class.
    Can be used for fetching the list of available Wi-Fi networks on Microsoft Windows platform.
    
    Based on the [pywifi project](https://github.com/awkman/pywifi) by Jiang Sheng-Jhih, licensed under the terms of the MIT license.
    
    Closes #47. Closes #55.

---
+++ wloc/fetchers/windows/native/__init__.py
@@ -0,0 +1,135 @@
+# coding=utf-8
+
+# SPDX-FileCopyrightText: 2017 Jiang Sheng-Jhih
+# SPDX-FileCopyrightText: 2021 EasyCoding Team
+#
+# SPDX-License-Identifier: MIT
+
+# Based on pywifi library: https://github.com/awkman/pywifi
+# License can be found in the licenses/pywifi.LICENSE.txt file.
+
+import ctypes
+import ctypes.wintypes
+
+from .structures import *
+
+
+class NativeWiFi:
+    """
+    Special class for working with the Windows Native Wi-Fi API.
+
+    MSDN: https://docs.microsoft.com/en-us/windows/win32/api/wlanapi/
+    """
+
+    @staticmethod
+    def _wlan_open_handle(client_version, negotiated_version, handle):
+        """
+        Python implementation of the WlanOpenHandle function from the Windows Native Wi-Fi API.
+
+        MSDN: https://docs.microsoft.com/en-us/windows/win32/api/wlanapi/nf-wlanapi-wlanopenhandle
+        :param client_version: The version of the highest WLAN API.
+        :param negotiated_version: The version of the WLAN API that will be used.
+        :param handle: Handle for the client.
+        :return: Return ERROR_SUCCESS on success.
+        """
+        func = ctypes.windll.wlanapi.WlanOpenHandle
+        func.argtypes = [ctypes.wintypes.DWORD, ctypes.c_void_p, ctypes.POINTER(ctypes.wintypes.DWORD),
+                         ctypes.POINTER(ctypes.wintypes.HANDLE)]
+        func.restypes = [ctypes.wintypes.DWORD]
+        return func(client_version, None, negotiated_version, handle)
+
+    @staticmethod
+    def _wlan_enum_interfaces(handle, ifaces):
+        """
+        Python implementation of the WlanEnumInterfaces function from the Windows Native Wi-Fi API.
+
+        MSDN: https://docs.microsoft.com/en-us/windows/win32/api/wlanapi/nf-wlanapi-wlanenuminterfaces
+        :param handle: The client's session handle.
+        :param ifaces: A pointer to the WLAN_INTERFACE_INFO_LIST structure.
+        :return: Return ERROR_SUCCESS on success.
+        """
+        func = ctypes.windll.wlanapi.WlanEnumInterfaces
+        func.argtypes = [ctypes.wintypes.HANDLE, ctypes.c_void_p,
+                         ctypes.POINTER(ctypes.POINTER(WLAN_INTERFACE_INFO_LIST))]
+        func.restypes = [ctypes.wintypes.DWORD]
+        return func(handle, None, ifaces)
+
+    @staticmethod
+    def _wlan_get_available_network_list(handle, iface_guid, network_list):
+        """
+        Python implementation of the WlanGetAvailableNetworkList function from the Windows Native Wi-Fi API.
+
+        MSDN: https://docs.microsoft.com/en-us/windows/win32/api/wlanapi/nf-wlanapi-wlangetavailablenetworklist
+        :param handle: The client's session handle.
+        :param iface_guid: GUID of the wireless interface to be queried.
+        :param network_list: A pointer to the WLAN_AVAILABLE_NETWORK_LIST structure.
+        :return: Return ERROR_SUCCESS on success.
+        """
+        func = ctypes.windll.wlanapi.WlanGetAvailableNetworkList
+        func.argtypes = [ctypes.wintypes.HANDLE, ctypes.POINTER(comtypes.GUID), ctypes.wintypes.DWORD, ctypes.c_void_p,
+                         ctypes.POINTER(ctypes.POINTER(WLAN_AVAILABLE_NETWORK_LIST))]
+        func.restypes = [ctypes.wintypes.DWORD]
+        return func(handle, iface_guid, 2, None, network_list)
+
+    @staticmethod
+    def _wlan_get_network_bss_list(handle, iface_guid, bss_list, ssid=None, security=False):
+        """
+        Python implementation of the WlanGetNetworkBssList function from the Windows Native Wi-Fi API.
+
+        MSDN: https://docs.microsoft.com/en-us/windows/win32/api/wlanapi/nf-wlanapi-wlangetnetworkbsslist
+        :param handle: The client's session handle.
+        :param iface_guid: GUID of the wireless interface to be queried.
+        :param bss_list: A pointer to the WLAN_BSS_LIST structure.
+        :param ssid: A pointer to a DOT11_SSID structure.
+        :param security: Indicates whether security is enabled on the network or not.
+        :return: Return ERROR_SUCCESS on success.
+        """
+        func = ctypes.windll.wlanapi.WlanGetNetworkBssList
+        func.argtypes = [ctypes.wintypes.HANDLE, ctypes.POINTER(comtypes.GUID), ctypes.POINTER(DOT11_SSID),
+                         ctypes.c_uint, ctypes.c_bool, ctypes.c_void_p, ctypes.POINTER(ctypes.POINTER(WLAN_BSS_LIST))]
+        func.restypes = [ctypes.wintypes.DWORD]
+        return func(handle, iface_guid, ssid, 1, security, None, bss_list)
+
+    def get_interfaces(self) -> list:
+        """
+        Gets the list of available Wi-Fi physical network interfaces.
+        :return: The list of available Wi-Fi physical network interfaces.
+        """
+        ifaces = []
+        self._wlan_open_handle(2, ctypes.byref(self._negotiated_version), ctypes.byref(self._handle))
+        self._wlan_enum_interfaces(self._handle, ctypes.byref(self._ifaces))
+        interfaces = ctypes.cast(self._ifaces.contents.InterfaceInfo, ctypes.POINTER(WLAN_INTERFACE_INFO))
+        for i in range(0, self._ifaces.contents.dwNumberOfItems):
+            iface = {'guid': interfaces[i].InterfaceGuid, 'name': interfaces[i].strInterfaceDescription}
+            ifaces.append(iface)
+        return ifaces
+
+    def get_networks(self, interface):
+        """
+        Gets the list of available Wi-Fi networks with their BSSID and signal strength.
+        :param interface: A physical network interface to use.
+        :return: The list of available Wi-Fi networks.
+        """
+        network_list = []
+        avail_network_list = ctypes.pointer(WLAN_AVAILABLE_NETWORK_LIST())
+        self._wlan_get_available_network_list(self._handle, ctypes.byref(interface['guid']),
+                                              ctypes.byref(avail_network_list))
+        networks = ctypes.cast(avail_network_list.contents.Network, ctypes.POINTER(WLAN_AVAILABLE_NETWORK))
+        for i in range(avail_network_list.contents.dwNumberOfItems):
+            if networks[i].dot11BssType == 1 and networks[i].bNetworkConnectable:
+                bss_list = ctypes.pointer(WLAN_BSS_LIST())
+                self._wlan_get_network_bss_list(self._handle, ctypes.byref(interface['guid']), ctypes.byref(bss_list),
+                                                networks[i].dot11Ssid, networks[i].bSecurityEnabled)
+                bsses = ctypes.cast(bss_list.contents.wlanBssEntries, ctypes.POINTER(WLAN_BSS_ENTRY))
+                for j in range(bss_list.contents.dwNumberOfItems):
+                    network_list.append(
+                        [":".join(["%02x" % bssid for bssid in bsses[j].dot11Bssid[0:6]]), bsses[j].lRssi])
+        return network_list
+
+    def __init__(self):
+        """
+        Main constructor of the NativeWiFi class.
+        """
+        self._negotiated_version = ctypes.wintypes.DWORD()
+        self._handle = ctypes.wintypes.HANDLE()
+        self._ifaces = ctypes.pointer(WLAN_INTERFACE_INFO_LIST())
---
+++ wloc/fetchers/windows/native/structures.py
@@ -0,0 +1,145 @@
+# coding=utf-8
+
+# SPDX-FileCopyrightText: 2017 Jiang Sheng-Jhih
+# SPDX-FileCopyrightText: 2021 EasyCoding Team
+#
+# SPDX-License-Identifier: MIT
+
+# Based on pywifi library: https://github.com/awkman/pywifi
+# License can be found in the licenses/pywifi.LICENSE.txt file.
+
+import ctypes.wintypes
+import comtypes
+
+
+class DOT11_SSID(ctypes.Structure):
+    """
+    Python prototype of the DOT11_SSID structure from the
+    Windows Native Wi-Fi API.
+
+    MSDN: https://docs.microsoft.com/en-us/windows/win32/api/wlanapi/
+    """
+    _fields_ = [("uSSIDLength", ctypes.c_ulong),
+                ("ucSSID", ctypes.c_char * 32)]
+
+
+class WLAN_RATE_SET(ctypes.Structure):
+    """
+    Python prototype of the WLAN_RATE_SET structure from the
+    Windows Native Wi-Fi API.
+
+    MSDN: https://docs.microsoft.com/en-us/windows/win32/api/wlanapi/
+    """
+    _fields_ = [
+        ("uRateSetLength", ctypes.c_ulong),
+        ("usRateSet", ctypes.c_ushort * 126)
+    ]
+
+
+class WLAN_AVAILABLE_NETWORK(ctypes.Structure):
+    """
+    Python prototype of the WLAN_AVAILABLE_NETWORK structure from the
+    Windows Native Wi-Fi API.
+
+    MSDN: https://docs.microsoft.com/en-us/windows/win32/api/wlanapi/
+    """
+    _fields_ = [
+        ("strProfileName", ctypes.c_wchar * 256),
+        ("dot11Ssid", DOT11_SSID),
+        ("dot11BssType", ctypes.c_uint),
+        ("uNumberOfBssids", ctypes.c_ulong),
+        ("bNetworkConnectable", ctypes.c_bool),
+        ("wlanNotConnectableReason", ctypes.c_uint),
+        ("uNumberOfPhyTypes", ctypes.c_ulong * 8),
+        ("dot11PhyTypes", ctypes.c_uint),
+        ("bMorePhyTypes", ctypes.c_bool),
+        ("wlanSignalQuality", ctypes.c_ulong),
+        ("bSecurityEnabled", ctypes.c_bool),
+        ("dot11DefaultAuthAlgorithm", ctypes.c_uint),
+        ("dot11DefaultCipherAlgorithm", ctypes.c_uint),
+        ("dwFlags", ctypes.wintypes.DWORD),
+        ("dwReserved", ctypes.wintypes.DWORD)
+    ]
+
+
+class WLAN_AVAILABLE_NETWORK_LIST(ctypes.Structure):
+    """
+    Python prototype of the WLAN_AVAILABLE_NETWORK_LIST structure from the
+    Windows Native Wi-Fi API.
+
+    MSDN: https://docs.microsoft.com/en-us/windows/win32/api/wlanapi/
+    """
+    _fields_ = [
+        ("dwNumberOfItems", ctypes.wintypes.DWORD),
+        ("dwIndex", ctypes.wintypes.DWORD),
+        ("Network", WLAN_AVAILABLE_NETWORK * 1)
+    ]
+
+
+class WLAN_BSS_ENTRY(ctypes.Structure):
+    """
+    Python prototype of the WLAN_BSS_ENTRY structure from the
+    Windows Native Wi-Fi API.
+
+    MSDN: https://docs.microsoft.com/en-us/windows/win32/api/wlanapi/
+    """
+    _fields_ = [
+        ("dot11Ssid", DOT11_SSID),
+        ("uPhyId", ctypes.c_ulong),
+        ("dot11Bssid", ctypes.c_ubyte * 6),
+        ("dot11BssType", ctypes.c_uint),
+        ("dot11BssPhyType", ctypes.c_uint),
+        ("lRssi", ctypes.c_long),
+        ("uLinkQuality", ctypes.c_ulong),
+        ("bInRegDomain", ctypes.c_bool),
+        ("usBeaconPeriod", ctypes.c_ushort),
+        ("ullTimestamp", ctypes.c_ulonglong),
+        ("ullHostTimestamp", ctypes.c_ulonglong),
+        ("usCapabilityInformation", ctypes.c_ushort),
+        ("ulChCenterFrequency", ctypes.c_ulong),
+        ("wlanRateSet", WLAN_RATE_SET),
+        ("ulIeOffset", ctypes.c_ulong),
+        ("ulIeSize", ctypes.c_ulong)
+    ]
+
+
+class WLAN_BSS_LIST(ctypes.Structure):
+    """
+    Python prototype of the WLAN_BSS_LIST structure from the
+    Windows Native Wi-Fi API.
+
+    MSDN: https://docs.microsoft.com/en-us/windows/win32/api/wlanapi/
+    """
+    _fields_ = [
+        ("dwTotalSize", ctypes.wintypes.DWORD),
+        ("dwNumberOfItems", ctypes.wintypes.DWORD),
+        ("wlanBssEntries", WLAN_BSS_ENTRY * 1)
+    ]
+
+
+class WLAN_INTERFACE_INFO(ctypes.Structure):
+    """
+    Python prototype of the WLAN_INTERFACE_INFO structure from the
+    Windows Native Wi-Fi API.
+
+    MSDN: https://docs.microsoft.com/en-us/windows/win32/api/wlanapi/
+    """
+    _fields_ = [
+        ("InterfaceGuid", comtypes.GUID),
+        ("strInterfaceDescription", ctypes.c_wchar * 256),
+        ("isState", ctypes.c_uint)
+    ]
+
+
+class WLAN_INTERFACE_INFO_LIST(ctypes.Structure):
+    """
+    Python prototype of the WLAN_INTERFACE_INFO_LIST structure from the
+    Windows Native Wi-Fi API.
+
+    MSDN: https://docs.microsoft.com/en-us/windows/win32/api/wlanapi/
+    """
+    _fields_ = [
+        ("dwNumberOfItems", ctypes.wintypes.DWORD),
+        ("dwIndex", ctypes.wintypes.DWORD),
+        ("InterfaceInfo", WLAN_INTERFACE_INFO * 1)
+    ]
