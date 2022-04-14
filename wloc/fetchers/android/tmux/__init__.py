c3ca7b2949bbf4ca64a51f19dad9cae483821b52
Vitaly Zaitsev <vitaly@easycoding.org>
2022-04-14 14:42:25 +0200

    Added full Android support (with Termux).
    Closes #57.

--- wloc/fetchers/android/tmux/__init__.py
+++ wloc/fetchers/android/tmux/__init__.py
@@ -4,10 +4,28 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import json
+import subprocess
+
+from ....exceptions import FetcherError
+
 
 class TermuxAPI:
+    def _fetch_json(self):
+        self._json = subprocess.check_output('termux-wifi-scaninfo', encoding='utf8', timeout=5000)
+
+    def _parse_json(self):
+        networks = json.loads(self._json)
+        if not isinstance(networks, list):
+            raise FetcherError(networks['error'])
+        for network in networks:
+            self._network_list.append([network['bssid'], network['rssi']])
+
     def get_networks(self):
-        pass
+        self._fetch_json()
+        self._parse_json()
+        return self._network_list
 
     def __init__(self):
-        pass
+        self._json = ''
+        self._network_list = []
