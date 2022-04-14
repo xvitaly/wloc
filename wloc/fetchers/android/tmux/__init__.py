537bd7cb4109764dadd4494898fb53dcf2e515fd
Vitaly Zaitsev <vitaly@easycoding.org>
2022-04-14 14:57:17 +0200

    Added Doxygen-style documentation to TermuxAPI class.

--- wloc/fetchers/android/tmux/__init__.py
+++ wloc/fetchers/android/tmux/__init__.py
@@ -11,21 +11,41 @@ from ....exceptions import FetcherError
 
 
 class TermuxAPI:
-    def _fetch_json(self):
+    """
+    Class for working with Termux API.
+    """
+
+    def _fetch_json(self) -> None:
+        """
+        Fetches JSON from stdout and stores the result in a special
+        private field.
+        """
         self._json = subprocess.check_output('termux-wifi-scaninfo', encoding='utf8', timeout=5000)
 
-    def _parse_json(self):
+    def _parse_json(self) -> None:
+        """
+        Parses JSON stored in a special private field and tries to
+        extract the required information from it (BSSID and RSSI).
+        :exception FetcherError: Termux API error message.
+        """
         networks = json.loads(self._json)
         if not isinstance(networks, list):
             raise FetcherError(networks['error'])
         for network in networks:
             self._network_list.append([network['bssid'], network['rssi']])
 
-    def get_networks(self):
+    def get_networks(self) -> list:
+        """
+        Gets the list of available Wi-Fi networks with their BSSID and signal strength.
+        :return: The list of available Wi-Fi networks.
+        """
         self._fetch_json()
         self._parse_json()
         return self._network_list
 
-    def __init__(self):
-        self._json = ''
-        self._network_list = []
+    def __init__(self) -> None:
+        """
+        Main constructor of the TermuxAPI class.
+        """
+        self._json: str = ''
+        self._network_list: list = []
