033da058ae6b8849256c5d0609cd8c0e00684002
Vitaly Zaitsev <vitaly@easycoding.org>
2022-04-14 12:49:00 +0200

    Added a new class TermuxAPI.

---
+++ wloc/fetchers/android/tmux/__init__.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+# SPDX-FileCopyrightText: 2015-2022 EasyCoding Team
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+class TermuxAPI:
+    def get_networks(self):
+        pass
+
+    def __init__(self):
+        pass
