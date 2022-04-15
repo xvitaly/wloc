b59d36736e631b5d217bd4407d2b70e810d828c3
Vitaly Zaitsev <vitaly@easycoding.org>
2022-04-15 13:46:14 +0200

    Added a new class NetworkManagerAPI for working with Network Manager on GNU/Linux.

---
+++ wloc/fetchers/linux/nm/__init__.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+# SPDX-FileCopyrightText: 2015-2022 EasyCoding Team
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+class NetworkManagerAPI:
+    def get_networks(self) -> list:
+        pass
+
+    def __init__(self) -> None:
+        pass
